1d7fe9217326086913ddb509a89301fc459d21ec
seon-24 <82245260+seon-24@users.noreply.github.com>
2021-04-11 22:40:34 +0900

    Updates 2 on Github branch only

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ print("Hello World Updates on Github")
 
 #Updates in local master branch
 print("Hello World Updates on local master branch")
+
+#Updates on mainbranch of Github
+print("Hello World Updates 2 on Github Only")
